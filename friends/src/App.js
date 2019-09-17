1cf5a20eb2ec16918a79d5314e14ba53702aa611
allisonkydy <allisnelly@gmail.com>
2019-09-17 14:54:04 -0500

    add login form and route

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Route path="/login" component={Login} />
     </div>
   );
 }
---
+++ friends/src/components/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({ username: "", password: "" })
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setCredentials({ username: "", password: "" })
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login;

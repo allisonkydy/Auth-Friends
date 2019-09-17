d5826af06dc59beb62d2fbe3af024118167b39ce
allisonkydy <allisnelly@gmail.com>
2019-09-17 15:28:44 -0500

    friends data fetched and displaying

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth().get('/friends')
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div>
-      friends go here
+      {friends.map(friend => {
+        return (
+          <div>
+            <h2>{friend.name}</h2>
+            <p>Age: {friend.age}</p>
+            <p>{friend.email}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }

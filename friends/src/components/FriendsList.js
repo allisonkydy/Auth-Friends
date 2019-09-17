6310f4d507510ce899f851da83d6194d9d45d4ff
allisonkydy <allisnelly@gmail.com>
2019-09-17 15:42:40 -0500

    add friend form built and posting new friend to server

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -3,6 +3,7 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsList = () => {
   const [friends, setFriends] = useState([]);
+  const [newFriend, setNewFriend] = useState({ name: "", age: "", email: "" })
 
   useEffect(() => {
     axiosWithAuth().get('/friends')
@@ -12,11 +13,56 @@ const FriendsList = () => {
       .catch(err => console.log(err))
   }, [])
 
+  const handleChange = e => {
+    setNewFriend({ ...newFriend, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth().post('/friends', { ...newFriend, id: Date.now() })
+      .then(res => {
+        setFriends(res.data)
+      })
+      .catch(err => console.log(err))
+    setNewFriend({ name: "", age: "", email: "" });
+  }
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        Add New Friend
+        <label>
+          Name: 
+          <input
+            type="text"
+            name="name"
+            value={newFriend.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Age: 
+          <input
+            type="number"
+            name="age"
+            value={newFriend.age}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Email: 
+          <input
+            type="email"
+            name="email"
+            value={newFriend.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Add friend</button>
+      </form>
       {friends.map(friend => {
         return (
-          <div>
+          <div key={friend.id}>
             <h2>{friend.name}</h2>
             <p>Age: {friend.age}</p>
             <p>{friend.email}</p>
